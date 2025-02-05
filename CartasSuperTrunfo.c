625474a62b4d72d60df7486b8c3a466244022a8a
Thiago Peixoto <104518821+ThiagoPeixoto81@users.noreply.github.com>
2025-02-05 18:52:36 +0000

    montando a estrutura

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,32 +4,85 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
+struct CartasSuperTrunfo
+{
+    // char estado;
+    char cardcode[255];
+    char nomeDaCidade[255];
+    unsigned long int populacao;
+    float areaEmKm;
+    int numeroPontosTuristicos;
+    float densidadePopulacional;
+    float pibPerCapita;
+    // float superPoder;
+
+};
+
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    int population01;
-    float area01;
-    float pib01;
-    int turismo01;
-    float density01;
-    float pibpercapita01;
-    float superpower02;
+    //Criando as cartas que serão utilizadas
+    struct CartasSuperTrunfo carta1;
+    struct CartasSuperTrunfo carta2;
+    struct CartasSuperTrunfo carta3;
+    struct CartasSuperTrunfo carta4;
+    int contador;
+
+    for(contador = 1; contador <= 4; contador++) {
+        //Criando um carta local
+        struct CartasSuperTrunfo cartaLocal;
+        char nomeDaCidadeLocal[255];
 
 
-    int population02;
-    float area02;
-    float pib02;
-    int turismo02;
-    float density02;
-    float pibpercapita02;
-    float superpower02;
+        printf("Insira o nome da cidade: ");
+        scanf("%s", &nomeDaCidadeLocal);
+        strcpy(cartaLocal.nomeDaCidade, nomeDaCidadeLocal );
+
+        printf("Insira a população: ");
+        scanf("%d", &cartaLocal.populacao);
+
+        printf("Insira a Área em Km²: ");
+        scanf("%f", &cartaLocal.areaEmKm);
+
+        printf("Insira o número de pontos turísticos: ");
+        scanf("%d", &cartaLocal.numeroPontosTuristicos);
+
+        printf("Insira a densidade populacional: ");
+        scanf("%f", &cartaLocal.densidadePopulacional);
+
+        printf("Insira o Pib per Capita: ");
+        scanf("%f", &cartaLocal.pibPerCapita);
+
+        switch (contador) {
+            case 1:
+                carta1 = cartaLocal;
+            break;
+
+            case 2:
+               carta2 = cartaLocal;
+            break;
+
+            case 3:
+                carta3 = cartaLocal;;
+            break;
+
+            case 4:
+                carta4 = cartaLocal;
+            break;
+
+            default:
+            break;
+        }
+
+        // cartaLocal.superPoder = cartaLocal.pibPerCapita + cartaLocal.densidadePopulacional + cartaLocal.numeroPontosTuristicos + cartaLocal.areaEmKm + cartaLocal.populacao;
+    };
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
